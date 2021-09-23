fb96c8fccc6f0ecc544e22fe1620cf5c1078bd0d
Raj Padmanabh <61701158+padmanabh007@users.noreply.github.com>
2021-09-23 17:27:45 +0530

    Update __init__.py

--- student/__init__.py
+++ student/__init__.py
@@ -3,10 +3,10 @@ from flask import Flask
 from flask_mysqldb import MySQL
 
 app=Flask(__name__)
-app.config['SECRET_KEY']=""
-app.config['MYSQL_HOST']=''
-app.config['MYSQL_USER']=''
-app.config['MYSQL_PASSWORD']=''
+app.config['SECRET_KEY']="1234455ugugfugkagjkgkjdhiuaht4y5i4itgui43htui4hjkfhuh"
+app.config['MYSQL_HOST']='stud123.mysql.database.azure.com'
+app.config['MYSQL_USER']='student@stud123'
+app.config['MYSQL_PASSWORD']='stud1@34'
 app.config['MYSQl_DB']='student'
 app.config['MYSQL_CURSORCLASS']='DictCursor'
 db=MySQL(app)
