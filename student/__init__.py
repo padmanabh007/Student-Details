6fde8588b547799fc41cce92c70bb67372b6ed6e
Raj Padmanabh <61701158+padmanabh007@users.noreply.github.com>
2021-09-26 17:29:24 +0530

    Project

---
+++ student/__init__.py
@@ -0,0 +1,19 @@
+from os import path
+from flask import Flask
+from flask_mysqldb import MySQL
+
+app=Flask(__name__)
+app.config['SECRET_KEY']="1234455ugugfugkagjkgkjdhiuaht4y5i4itgui43htui4hjkfhuh"
+app.config['MYSQL_HOST']='stud123.mysql.database.azure.com'
+app.config['MYSQL_USER']='student@stud123'
+app.config['MYSQL_PASSWORD']='stud1@34'
+app.config['MYSQl_DB']='student'
+app.config['MYSQL_CURSORCLASS']='DictCursor'
+db=MySQL(app)
+
+    
+
+from .auth import auth
+from .views import views
+app.register_blueprint(auth,url_prefix='/')
+app.register_blueprint(views,url_prefix='/')
---
+++ student/forms.py
@@ -0,0 +1,20 @@
+from  flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,SubmitField
+from wtforms.validators import Email,Length,DataRequired,EqualTo
+
+#Create a form class
+class RegisterForm(FlaskForm):
+    email = StringField('Email',validators=[DataRequired(),Email()])
+    password = PasswordField('Password',validators=[DataRequired(),Length(min=8,max=16)])
+    confirm = PasswordField('Confirm-Password',validators=[DataRequired(), EqualTo('password')])
+    signup = SubmitField('SignUp')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',validators=[DataRequired(),Email()])
+    password = PasswordField('Password',validators=[DataRequired()])
+    signin = SubmitField('SignIn')
+
+class AdminLogin(FlaskForm):
+    userid = StringField('Email',validators=[DataRequired(),Email()])
+    password = PasswordField('Password',validators=[DataRequired()])
+    signin = SubmitField('SignIn')
